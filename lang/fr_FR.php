ae88d45e6a304e2944fbcb0034b3991a4d869bd4
Jean Molliné <jmolline@gmail.com>
2021-06-18 09:05:49 +0200

    Correction petite faute dans le fichier de messages en français.

--- lang/fr_FR.php
+++ lang/fr_FR.php
@@ -5,7 +5,7 @@
 $aMessages = array(
     'err_mkdir'     => "Erreur lors de la création du répertoire de destination « %s »",
     'err_upload_1'  => "La taille du fichier téléchargé excède la valeur de upload_max_filesize (%s)",
-    'err_upload_2'  => "La taille du fichier téléchargé excède la valeur de qui a été spécifiée dans le formulaire HTML (%s).",
+    'err_upload_2'  => "La taille du fichier téléchargé excède la valeur qui a été spécifiée dans le formulaire HTML (%s).",
     'err_upload_3'  => "Le fichier n'a été que partiellement téléchargé.",
     'err_upload_4'  => "Aucun fichier n'a été téléchargé.",
     'err_upload_6'  => "Un répertoire temporaire de stockage est manquant ou introuvable.",
